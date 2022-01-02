d69b9c3c084c25096d93caa73eff242d4f2764a8
Yogesh <yogesh.grjr@rediffmail.com>
2022-01-02 11:57:26 -0600

    Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,10 @@ import image from "../images/photo-grid.png"
 
 export default function Navbar(){
     return (
-        <div className='hero'>
-            <img src = {image} />
-            <h1>Online Experiences</h1>
-            <p1>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p1>
-        </div>
+        <section className="hero">
+            <img src = {image} className='hero--img'/>
+            <h1 className='hero--header'>Online Experiences</h1>
+            <p1 className='hero--text'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p1>
+        </section>
     )
 }
